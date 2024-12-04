6c5bcd6b07e8120894b665520cc642feaebfe7a5
heymiced34@gmail.com <alumnos@alumnosuio.ute.edu.ec>
2024-12-03 20:09:23 -0500

    Calcule subtotal

--- EJERCICIO 23/utilitarios.py
+++ EJERCICIO 23/utilitarios.py
@@ -2,3 +2,15 @@ def calcularPromedio(val1,val2,val3):
     calculo=val1+val2+val3
     promedio1=calculo/3
     return promedio1
+
+def calcularSubtotal(pr,ca,des):
+    subtotal1 =pr*ca
+    desu= des*subtotal1/100
+    res=subtotal1-desu
+
+    return res
+    
+
+    
+    
+    
